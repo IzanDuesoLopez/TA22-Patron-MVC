ac27599606c64590305fec8cc51ab363895724ab
IzanDuesoLopez <izandueso@gmail.com>
2022-08-08 10:04:09 +0200

    Commit inicial

---
+++ TA22_Ejercicio1/src/main/java/TA22_Ejercicio1/TA22_Ejercicio1/App.java
@@ -0,0 +1,13 @@
+package TA22_Ejercicio1.TA22_Ejercicio1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
